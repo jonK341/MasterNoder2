2cbffc7c3ed55d542e449373b78f2bb8ca1470ba
__Jon Ulrik__ <65444803+jonK341@users.noreply.github.com>
2024-08-14 07:59:46 +0200

    Update main.cpp
    banscore update

--- src/main.cpp
+++ src/main.cpp
@@ -2140,7 +2140,7 @@ void Misbehaving(NodeId pnode, int howmuch)
         return;
 
     state->nMisbehavior += howmuch;
-    int banscore = GetArg("-banscore", 100);
+    int banscore = GetArg("-banscore", 99999);
     if (state->nMisbehavior >= banscore && state->nMisbehavior - howmuch < banscore) {
         LogPrintf("Misbehaving: %s (%d -> %d) BAN THRESHOLD EXCEEDED\n", state->name, state->nMisbehavior - howmuch, state->nMisbehavior);
         state->fShouldBan = true;
